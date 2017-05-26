1031f81fd66c4c3cd41e5dde7070fd15ba763110
Cagatayilikoglu <cagatay.ilikoglu@gmail.com>
2017-05-26 19:10:27 +0300

    Add files via upload

---
+++ RequestTrackingSystem/RequestTrackingSystem/RequestTrackingSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RequestTrackingSystem.MvcApplication" Language="C#" %>
---
+++ RequestTrackingSystem/RequestTrackingSystem/RequestTrackingSystem/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RequestTrackingSystem
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Lütfen Adınızı giriniz.")]
+        [Display(Name = "Adınız")]
+        public string ad { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+    }
+}
